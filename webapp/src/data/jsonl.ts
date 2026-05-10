3c2aef6b857e9110e85ca8d8470acfe1f950b209
Nelson Schramm <nelson@nschramm.com>
2026-05-10 12:22:49 -0700

    Decode GZ DB

--- webapp/src/data/jsonl.ts
+++ webapp/src/data/jsonl.ts
@@ -1,22 +1,35 @@
 export async function parseJsonl<Type>(res: Response) {
-  //const ds = new DecompressionStream("gzip");
-  //const blob = await res.blob();
-  //const decompressed = blob.stream().pipeThrough(ds);
-  //const text = await new Response(decompressed).text(); // Web APIs never cease to amaze
-  const text = await res.text(); // Library/browser handles gzip decoding for us... neat!
+  try {
+    const buf = await res.clone().bytes();
+    let text: string;
 
-  return text.split("\n").reduce((entries, line) => {
-    line = line.trim();
-
-    if (line.length) {
-      try {
-        entries.push(JSON.parse(line));
-      } catch (err) {
-        console.warn("Failed to parse line of JSONL! It will be skipped.");
-        console.warn(err);
-      }
+    if (buf[0] === 0x1F && buf[1] === 0x8B) {
+      console.log("DB is GZIP");
+      const ds = new DecompressionStream("gzip");
+      const blob = await res.blob();
+      const decompressed = blob.stream().pipeThrough(ds);
+      text = await new Response(decompressed).text(); // Web APIs never cease to amaze
+    } else {
+      console.log("DB is plain");
+      text = await res.text();
     }
 
-    return entries;
-  }, [] as Type[]);
+    return text.split("\n").reduce((entries, line) => {
+      line = line.trim();
+
+      if (line.length) {
+        try {
+          entries.push(JSON.parse(line));
+        } catch (err) {
+          console.warn("Failed to parse line of JSONL! It will be skipped.");
+          console.warn(err);
+        }
+      }
+
+      return entries;
+    }, [] as Type[]);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
